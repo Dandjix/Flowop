659fb94693ef7061ea9e74118ae84b484b60a113
Timon Dubreuil <timondubreuil@outlook.com>
2024-11-30 13:09:48 +0100

    debut visqueux

--- Assets/Joueur/EtatsPhysiques/EtatPhysiqueVisqueux.cs
+++ Assets/Joueur/EtatsPhysiques/EtatPhysiqueVisqueux.cs
@@ -6,6 +6,37 @@ namespace EtatsPhysiques
     {
         [SerializeField] private GameObject joueurVisqueux;
 
+        private Vector2 Center { get
+            {
+                var joueur = joueurVisqueux.GetComponent<JoueurVisqueux>();
+                var bones = joueur.GetSortedBones();
+
+                Vector2 posSum = Vector2.zero;
+
+                foreach (var bone in bones)
+                {
+                    posSum = posSum + (Vector2)bone.position;
+                }
+
+                var posAvg = posSum/bones.Count;
+
+                return posAvg;
+            }
+            set
+            {
+                Vector2 blobPosition = Center;
+
+                var joueur = joueurVisqueux.GetComponent<JoueurVisqueux>();
+                var bones = joueur.GetSortedBones();
+
+                foreach (var bone in bones)
+                {
+                    Vector2 localPos = blobPosition - (Vector2)bone.transform.position;
+                    bone.position = value + localPos;
+                }
+            }
+        }
+
         protected override void enter(EtatPhysiqueState from)
         {
 
