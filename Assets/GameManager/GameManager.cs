0597df2b2687c85104434d3721534b5919f41943
EnzoBellicaud <114161120+EnzoBellicaud@users.noreply.github.com>
2024-12-01 15:48:30 +0100

    correct reload

--- Assets/GameManager/GameManager.cs
+++ Assets/GameManager/GameManager.cs
@@ -5,6 +5,7 @@ public class GameManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private float hBound, vBound;
+    public static GameManager Instance;
     
     public float startTime;
     public static float timeElapsed;
@@ -17,14 +18,30 @@ public class GameManager : MonoBehaviour
         
     }
 
+
+     private void Awake()
+    {
+        // Assurez-vous qu'il n'y a qu'une seule instance du GameManager
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Optionnel, si vous voulez que GameManager persiste entre les scènes
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
             Restart();
-             timeElapsed = Time.time - startTime;
-             SceneManager.LoadScene("Winner");
+            //  timeElapsed = Time.time - startTime;
+            //  SceneManager.LoadScene("Winner");
         }
         Vector2 joueurPos = transform.position;
         if (Mathf.Abs(joueurPos.x) > hBound || Mathf.Abs(joueurPos.y) > vBound)
